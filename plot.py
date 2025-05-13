852135d627e2371698d8d23cf6d3e1e0ce7b4b3c
jlhb1984 <jlhb1984@gmail.com>
2025-05-12 23:28:35 -0500

    Se confirmó que los coeficientes están bien calculados, el problema está en que la función tal y como está definida, no permite una buena aproximación con series de Fourier.

--- plot.py
+++ plot.py
@@ -11,21 +11,22 @@ serie_f=[]
 T=255
 w=(2*pi)/T
 
-a0=1/65025
+a0=1/T
 n=1
-tiempo=0.1
+print(np.cos(pi))
 for i in range(0,5):
-    #an[i]=
-    an.append((-(1/(n*pi)))*(math.sin((2/255)*pi*n)-math.sin(0.0078*pi*n)))
-    bn.append(((1/(n*pi)))*(math.cos((2/255)*pi*n)-math.cos(0.0078*pi*n)))
+    an.append((( 1/(n*pi))) * (math.sin((2*pi*n)/(255*255))))    
+    bn.append(((1/(n*pi))) * (1-math.cos((2*pi*n)/(255*255))))
     n=n+1
-print(an)
-print(bn)
+    print(an[i])
+    print(bn[i])
 
-t=np.linspace(0,514,1000)
-#y=(an[0]*math.sin(w*t*1)+bn[0]*math.cos(w+t*1))
+t=np.linspace(0,4096,100000)
 y=(a0+an[0]*np.cos(w*t*1)+bn[0]*np.sin(w*t*1)+
       an[1]*np.cos(w*t*2)+bn[1]*np.sin(w*t*2)+
-      an[2]*np.cos(w*t*3)+bn[2]*np.sin(w*t*3))
+      an[2]*np.cos(w*t*3)+bn[2]*np.sin(w*t*3)+
+      an[3]*np.cos(w*t*4)+bn[3]*np.sin(w*t*4)+
+      an[4]*np.cos(w*t*5)+bn[4]*np.sin(w*t*5)
+      )
 plt.plot(t,y)
 plt.show()
